55e94f84e2aa6507370beeb6e7f084fb52b4a059
Lucas Daniel Alves Vieira <ldanivieira@gmail.com>
2020-09-14 19:27:33 -0400

    flutter init

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@gmail.com',
+    avatarUrl: 'https://pixabay.com/vectors/avatar-beak-black-cute-emotion-1295429/',
+  ),
+  '2': const User(
+    id: '2',
+    name: 'João',
+    email: 'joão@gmail.com',
+    avatarUrl: 'https://pixabay.com/vectors/man-person-avatar-face-head-156584/',
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Junior',
+    email: 'junior@gmail.com',
+    avatarUrl: 'https://pixabay.com/vectors/female-portrait-avatar-face-head-306407/',
+  ),
+};
